8d99626079001db64688f2011b6e1f17fa7d33d1
Marie-Pierre Lessard <male9@elev.techcollege.dk>
2026-02-12 13:49:24 +0100

    Problem with since (year)

--- _ROOT/src/pages/since-that-year.tsx
+++ _ROOT/src/pages/since-that-year.tsx
@@ -1,3 +1,19 @@
+/* The API documentation by MuffinLabs at 
+https://history.muffinlabs.com/?utm_source=apislist.com
+does not reveal how to fetch historical tidbits from a certain year.
+I read and understood the JavaScript code at 
+https://history.muffinlabs.com/api.js and
+https://github.com/muffinista/really-simple-history-api/blob/main/public/api.js
+and it doesn't say anything about the endpoint that could fetch that data. 
+Also nothing at
+https://github.com/muffinista/really-simple-history-api/blob/main/public/ticker.js
+
+I even tried reading the old Ruby file (even though we are not supposed to have to learn Ruby).
+https://github.com/muffinista/really-simple-history-api/blob/main/old/parse-wikipedia.rb
+I could be wrong, but I don't think that that version of the API included that functionality either
+because the terms event_year and last_year are used, not by_year, for instance. 
+*/
+
 /* Source for the creation of pages:
 react-router-codealong-med-kasper
 */
